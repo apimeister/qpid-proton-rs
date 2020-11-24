006b1f5b11c2550727eb63a7d73c7a597ce7178b
Jens Walter <jens@apimeister.com>
2020-11-24 17:08:04 +0100

    version bump

--- examples/send_message_to_servicebus.rs
+++ examples/send_message_to_servicebus.rs
@@ -1,11 +1,11 @@
 use qpid_proton::*;
 
 fn main() {
-  let host = "servicebus4infrastructurecore.servicebus.windows.net".to_string();
+  let host = "testservicebus12345.servicebus.windows.net".to_string();
   let port = 5671;
   let auth = SaslAuth{
     username: "RootManageSharedAccessKey".to_string(),
-    password: "Hgg+bSZstkJIqhRpjixF+fSUjs+seRrsPvyMJFtrpuI=".to_string()
+    password: "Hgg+bSZstkJIqhRpjixF+fSUj".to_string()
   };
   let queue = "myqueue".to_string();
   let connection = connect(host,port,Some(auth),queue);
